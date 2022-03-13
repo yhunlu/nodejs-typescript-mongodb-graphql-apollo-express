b3b4b93a3412f5dd65bf0476fa0c07bdb2bce0e9
yhunlu <yhunlu87@gmail.com>
2022-03-14 02:49:57 +0300

    initial setup typescript-apolloexpress-graphql

---
+++ backend/src/graphql.ts
@@ -0,0 +1,23 @@
+import { GraphQLObjectType, GraphQLSchema, GraphQLString } from "graphql";
+
+const query = new GraphQLObjectType({
+    name: 'Query',
+    fields: {
+        hello: {
+            type: GraphQLString,
+            resolve: () => "Hello from the Query"
+        }
+    }
+});
+
+const mutation = new GraphQLObjectType({
+    name: 'Mutation',
+    fields: {
+        hello: {
+            type: GraphQLString,
+            resolve: () => "Hello from the Mutation"
+        }
+    }
+})
+
+export const schema = new GraphQLSchema({ query, mutation });
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,27 +1,22 @@
-import bodyParser from 'body-parser';
+import { ApolloServer } from 'apollo-server-express';
 import express from 'express';
-import { listings } from './listings';
+import { schema } from './graphql';
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use(bodyParser.json());
 
-app.get("/listings", (_req, res) => {
-  return res.send(listings);
-})
-
-app.post("/delete-listing", (req, res) => {
-  const id: string = req.body.id;
-
-  for (let i=0; i < listings.length; i++) {
-    if (listings[i].id === id) {
-      return res.send(listings.splice(i, 1));
-    }
+const server = new ApolloServer({ schema });
+const startApollo = async () => {
+  try {
+    await server.start();
+    server.applyMiddleware({ app, path: "/api"})
+  } catch (error) {
+    console.log(error);
   }
+}
 
-  return res.send('failed to delete listing');
-})
+startApollo();
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
